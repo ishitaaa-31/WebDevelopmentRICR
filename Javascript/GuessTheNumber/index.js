9d44737328be16719ac970c5f1624084dcbb0483
ishitaaa-31 <31ishitabajpai@gmail.com>
2025-12-13 20:36:33 +0530

    t

--- Javascript/GuessTheNumber/index.js
+++ Javascript/GuessTheNumber/index.js
@@ -1,26 +1,24 @@
-const secretNumber = Math.floor(Math.random() * 10) + 1;
-console.log("Secret Number:", secretNumber); // for testing
+const secretNumber = Math.floor(Math.random() * 20) + 1;
+console.log("Secret Number:", secretNumber);
 
 function checkGuess() {
   const guess = Number(document.getElementById("guess").value);
   const message = document.getElementById("message");
 
-  if (!guess || guess < 1 || guess > 10) {
-    message.innerText = "Please enter a number between 1 and 10.";
+  if (!guess || guess < 1 || guess > 20) {
+    message.innerText = "Please enter a number between 1 and 20.";
     message.className = "text-danger";
     return;
   }
 
   if (guess > secretNumber) {
-    message.innerText = "Oops! Sorry Try a Smaller Number";
+    message.innerText = " Try a Smaller Number";
     message.className = "text-warning";
-  } 
-  else if (guess < secretNumber) {
-    message.innerText = "Oops! Sorry Try a Larger Number";
+  } else if (guess < secretNumber) {
+    message.innerText = " Try a Larger Number";
     message.className = "text-warning";
-  } 
-  else {
-    message.innerText = "Congratulations! You have choosen the right number.";
+  } else {
+    message.innerText = "Congratulations!";
     message.className = "text-success";
   }
 }
