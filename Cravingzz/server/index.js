ad0d190be7cf01a8216fca73e96e015526931cbe
ishitaaa-31 <31ishitabajpai@gmail.com>
2026-01-12 19:02:02 +0530

    progress

--- Cravingzz/server/index.js
+++ Cravingzz/server/index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./src/config/db.js";
 
 const app = express();
+app.use(cors({origin: "http://localhost:5173"}))
 app.use(express.json());
 // app.use("/auth", AuthRouter);
 
