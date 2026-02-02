a2cec3e9366727dfff9d75f750779afe85c40007
ishitaaa-31 <31ishitabajpai@gmail.com>
2026-02-02 14:22:10 +0530

    restaurant dashboard

--- Cravingzz/client/src/pages/dashboards/RestaurantDashboard.jsx
+++ Cravingzz/client/src/pages/dashboards/RestaurantDashboard.jsx
@@ -1,9 +1,47 @@
-import React from 'react'
+import React, { useState ,useEffect} from "react";
+import Sidebar from "../../components/userDashboard/Sidebar";
+import Overview from "../../components/userDashboard/Overview";
+import Profile from "../../components/userDashboard/Profile";
+import Order from "../../components/userDashboard/Order";
+import Transaction from "../../components/userDashboard/Transaction";
+import HelpDesk from "../../components/userDashboard/HelpDesk";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 const RestaurantDashboard = () => {
+  const { role, isLogin } = useAuth();
+    const [active, setActive] = useState("overview");
+    const navigate = useNavigate();
+    const [isCollapsed, setCollapsed] = useState(false);
+  
+    useEffect(() => {
+      if (!isLogin) {
+        navigate("/login");
+      }
+    });
   return (
-    <div>RestaurantDashboard</div>
+    <>
+<div className="w-full flex  h-[90vh]">
+        <div
+          className={`bg-(--color-background) ${isCollapsed ? "w-2/60" : "w-12/60"} duration-300`}
+        >
+          <Sidebar
+            active={active}
+            setActive={setActive}
+            isCollapsed={isCollapsed}
+            setCollapsed={setCollapsed}
+          />
+        </div>
+        <div className={`${isCollapsed ? "w-58/60" : "w-48/60"} duration-300`}>
+          {active === "overview" && <Overview />}
+          {active === "profile" && <Profile />}
+          {active === "order" && <Order />}
+          {active === "transaction" && <Transaction />}
+          {active === "helpdesk" && <HelpDesk />}
+        </div>
+      </div>
+    </>
   )
 }
 
-export default RestaurantDashboard
+export default RestaurantDashboard;
--- Cravingzz/client/src/pages/dashboards/RiderDashboard.jsx
+++ Cravingzz/client/src/pages/dashboards/RiderDashboard.jsx
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { useState ,useEffect} from "react";
+import Sidebar from "../../components/userDashboard/Sidebar";
+import Overview from "../../components/userDashboard/Overview";
+import Profile from "../../components/userDashboard/Profile";
+import Order from "../../components/userDashboard/Order";
+import Transaction from "../../components/userDashboard/Transaction";
+import HelpDesk from "../../components/userDashboard/HelpDesk";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
 
 const RiderDashboard = () => {
+  const { role, isLogin } = useAuth();
+    const [active, setActive] = useState("overview");
+    const navigate = useNavigate();
+    const [isCollapsed, setCollapsed] = useState(false);
+  
+    useEffect(() => {
+      if (!isLogin) {
+        navigate("/login");
+      }
+    });
   return (
-    <div>RiderDashboard</div>
+    <>
+<div className="w-full flex  h-[90vh]">
+        <div
+          className={`bg-(--color-background) ${isCollapsed ? "w-2/60" : "w-12/60"} duration-300`}
+        >
+          <Sidebar
+            active={active}
+            setActive={setActive}
+            isCollapsed={isCollapsed}
+            setCollapsed={setCollapsed}
+          />
+        </div>
+        <div className={`${isCollapsed ? "w-58/60" : "w-48/60"} duration-300`}>
+          {active === "overview" && <Overview />}
+          {active === "profile" && <Profile />}
+          {active === "order" && <Order />}
+          {active === "transaction" && <Transaction />}
+          {active === "helpdesk" && <HelpDesk />}
+        </div>
+      </div>
+    </>
   )
 }
 
